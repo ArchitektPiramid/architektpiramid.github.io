c275a550faa43b8d836ff1f320d6ba6fefeab589
Dominik Nowak <architektpiramid@gmail.com>
2024-02-24 15:14:40 +0100

    feat: config files, metadata and some content

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,6 @@
  */
 const nextConfig = {
 	output: 'export',
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,45 @@
 import Link from 'next/link';
 import React from 'react';
+import Image from 'next/image';
+import todoIcon from '../../public/in_progress.svg';
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<h1 className="text-3xl font-bold">IN PROGRESS...</h1>
-			<button
-				type="button"
-				className="px-8 py-3 font-semibold rounded dark:bg-gray-100 dark:text-gray-800"
-			>
-				<Link href="/about">ABOUT subpage</Link>
-			</button>
+		<main className="flex space-y-12 flex-col items-center justify-between">
+			<h1 className="animate-pulse text-3xl font-bold mt-8">
+				IN PROGRESS...
+			</h1>
+			<div>
+				<h1 className="font-semibold text-2xl px-24 pb-2">
+					Version v0.1
+				</h1>
+				<ul
+					role="list"
+					className="marker:text-sky-400 list-disc pl-5 leading-2 text-slate-400"
+				>
+					<li className="line-through marker:text-lime-400 decoration-stone-200">
+						repair and config tailwindcss package
+					</li>
+					<li className="line-through">add basic layout metadata</li>
+					<li>fix Footer component</li>
+				</ul>
+			</div>
+			<div className="py-12 space-x-3">
+				<button
+					type="button"
+					className="uppercase hover:font-black hover:bg-gray-50 px-8 h-12 font-bold rounded dark:bg-gray-100 dark:text-gray-800"
+				>
+					<Link href="/about">About</Link>
+				</button>
+
+				<button
+					type="button"
+					disabled
+					className="px-8 h-12 disabled:line-through disabled:bg-gray-500 bg-gray-100 text-gray-800 rounded font-bold uppercase"
+				>
+					Projects
+				</button>
+			</div>
 		</main>
 	);
 }
