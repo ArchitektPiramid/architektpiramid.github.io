6f25877a7934796c092735fea540c9d4fc78e4f9
Dominik Nowak <architektpiramid@gmail.com>
2024-02-24 15:14:40 +0100

    feat: config files, metadata and some content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,7 @@ import { useRouter } from 'next/navigation';
 export default function Page() {
 	const router = useRouter();
 	return (
-		<div className='className="flex min-h-screen flex-col items-center justify-between p-24"'>
+		<div className="flex min-h-screen flex-col items-center justify-between p-24">
 			<h1>About me page. Router: /about</h1>
 			<button
 				type="button"
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
+import type { Metadata } from 'next';
 import Footer from '@/components/Footer';
 import '@/styles/globals.css';
 
+export const metadata: Metadata = {
+	title: 'ArchitektPiramid page',
+	description: 'Page containing blog/portfolio. Belongs to Dominik Nowak',
+};
+
 export default function RootLayout({
 	children,
-}: {
+}: Readonly<{
 	children: React.ReactNode;
-}) {
+}>) {
 	return (
 		<html lang="en">
 			<head></head>
