d024d657ed64fc3353d094a90006bfc6dcb95d1b
Nowak Dominik <architektpiramid@gmail.com>
2024-03-20 01:02:44 +0100

    wip: header navbar component

---
+++ src/components/header/RootHeader.tsx
@@ -0,0 +1,36 @@
+import HeaderNavbarComponent from './headerNavbar';
+import HeaderLogoComponent from './headerLogo';
+import isDEBUG from 'src/utils/helper';
+import { cn } from 'src/lib/utils';
+
+const navHeaderLinks: IHeaderNavLinks[] = [
+	{ name: 'Home', href: '/' },
+	{ name: 'Projects', href: '/projects' },
+	{ name: 'About me', href: '/about' },
+	{ name: 'Contact', href: '/contact' },
+];
+
+const RootHeader: React.FC = () => {
+	return (
+		<header
+			className={cn(
+				'sticky top-0 bg-gray-950 flex flex-wrap items-center justify-between mx-auto pt-4 pb-2 border-b-2 border-gray-600',
+				isDEBUG() && 'border-dashed border-2 border-sky-500',
+			)}
+		>
+			{/* <div>
+					<h1>#01</h1>
+					<h2>#02</h2>
+					<h3>#03</h3>
+				</div> */}
+			<HeaderLogoComponent />
+			<HeaderNavbarComponent valuesList={navHeaderLinks} />
+		</header>
+		// <header className="flex flex-wrap items-center mx-auto justify-between pt-4 border-b-2 border-gray-600 bg-gray-950">
+		// 	<LogoComp />
+		// 	<NavbarComp valuesList={navHeaderLinks} />
+		// </header>
+	);
+};
+
+export default RootHeader;
---
+++ src/components/header/headerLogo.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from 'next/link';
+import LogoIcon from 'src/icons/logo-icon';
+import { cn } from 'src/lib/utils';
+import isDEBUG from 'src/utils/helper';
+
+const HeaderLogoComponent: React.FC = () => {
+	const now = new Date();
+	const time =
+		now.getHours() + ' ' + now.getMinutes() + ':' + now.getSeconds();
+	return (
+		<div
+			className={cn(
+				'flex flex-wrap p-4',
+				'items-center justify-between mx-auto',
+				isDEBUG() && 'border-dashed border-2 border-orange-500',
+			)}
+		>
+			<Link href="/" className="flex items-center space-x-3">
+				<LogoIcon
+					fill={'white'}
+					width={60}
+					height={60}
+					scale={'4px'}
+					className="animate-spin"
+				/>
+				<span className="self-center text-2xl">Portfolio Page</span>
+				<p className="">Test4. Time: {time}</p>
+			</Link>
+		</div>
+	);
+};
+
+export default HeaderLogoComponent;
