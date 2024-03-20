6073f19c1385d34ad4e3bd991665a1445b87173d
Nowak Dominik <architektpiramid@gmail.com>
2024-03-20 01:03:30 +0100

    removed db project

--- src/app/projects/(project-stuff)/electro-db/category/[categoryId]/subcategory/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function SubcategoryPageMain() {
-	return (
-		<>
-			<h1>ELECTRO DB --- CATEGORY / SUBCAT page </h1>
-		</>
-	);
-}
--- src/app/projects/(project-stuff)/electro-db/category/categoryTypes.ts
+++
@@ -1,9 +0,0 @@
-interface ICategory {
-	id: string;
-	name: string;
-	category: string;
-	description?: string;
-	price?: number;
-	purchaseDate?: string; // np. w formacie "YYYY-MM-DD"
-	// Dodaj inne pola w zależności od potrzeb
-}
